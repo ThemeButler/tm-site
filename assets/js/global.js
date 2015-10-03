825ce44b7e1eea5a8df30e56db5fe0a80a933ad1
Chris Rault <chris@devignerforhire.com>
2015-10-03 12:09:34 +0200

    Updates

--- assets/js/global.js
+++
@@ -1,9 +0,0 @@
-!(function( $ ) {
-
-       $(".tm-header button").click(function () {
-         $(this).text(function(i, v){
-            return v === 'Show Navigation' ? 'Hide Navigation' : 'Show Navigation'
-         })
-       });
-
-})( window.jQuery );
